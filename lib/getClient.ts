0391e34ade293e81636ddac4e3ea35f7d9e60ca2
Hossam-Exe <58437507+Hossam-Exe@users.noreply.github.com>
2023-12-16 19:38:11 +0200

    Add files via upload

---
+++ lib/getClient.ts
@@ -0,0 +1,25 @@
+import { createClient } from "@sanity/client";
+
+import imageUrlBuilder from '@sanity/image-url';
+
+
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID 
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET 
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION 
+const token =process.env.SANITY_READ_TOKEN
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn:false,
+  token,
+  
+});
+
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: any) {
+  return builder.image(source)
+}
