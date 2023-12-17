4359fd5d310d68cec5130b16ae99861a457953d4
Hossam-Exe <58437507+Hossam-Exe@users.noreply.github.com>
2023-12-17 21:34:39 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,13 @@ export default function RootLayout({
   
   return (
     <html lang="en">
-    
+
+      <head>
+    <script async
+     src={`"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+     crossOrigin="anonymous"
+     />
+    </head>
     
       <body className={inter.className} >
 
