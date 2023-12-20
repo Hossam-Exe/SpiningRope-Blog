22eb42d62e3acd83bff8c2b9f970243f0cfe8101
Hossam-Exe <58437507+Hossam-Exe@users.noreply.github.com>
2023-12-21 00:07:35 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const postsQuery = groq`
 `;
 
 
-//export const revalidate = 60 // revalidate at most every 1 hour
+export const revalidate = 60 // revalidate at most every 1 hour
 
 
 export default async function Home() {
