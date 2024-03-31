6e881fa7bacdfe7ed345bd2ab3e2effe3661b63d
Hossam-Exe <58437507+Hossam-Exe@users.noreply.github.com>
2024-03-31 19:38:40 +0200

    Update Switcher.jsx

--- app/Components/Switcher.jsx
+++ app/Components/Switcher.jsx
@@ -5,7 +5,7 @@ import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 export default function Switcher() {
   const [colorTheme, setTheme] = useDarkSide();
-  const [darkSide, setDarkSide] = useState(colorTheme === 'light' ? true : false);
+  const [darkSide, setDarkSide] = useState(colorTheme === 'dark' ? true : false);
 
   const toggleDarkMode = checked => {
     setTheme(colorTheme);
